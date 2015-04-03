0e0e94921e878d96050bbf8addd7dbeb03876b9b
xiaopangniuniu <chenjing.tju@gmail.com>
2015-04-03 18:13:30 +0800

    update another method.

--- BigEndian/main.cpp
+++ BigEndian/main.cpp
@@ -10,12 +10,31 @@ bool isBigEndian()
 		return true;
 	return false;
 }
+
+bool isBigEndian_union()
+{
+	union NUM
+	{
+		int a;
+		char b;
+	} num;
+	num.a = 0x1234;
+	if (num.b == 0x12)
+		return true;
+	return false;
+}
+
 int main()
 {
 	if (isBigEndian())
 		std::cout << "Is big-endian" << std::endl;
 	else
 		std::cout << "Is little-endian" << std::endl;
+		
+	if (isBigEndian_union())
+		std::cout << "UNION: Is big-endian" << std::endl;
+	else
+		std::cout << "UNION: Is little-endian" << std::endl;
 		
 	return 0;
 }
